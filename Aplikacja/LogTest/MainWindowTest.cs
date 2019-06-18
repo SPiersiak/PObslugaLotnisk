d3e7d998347635666054c1d103acfe82a61a469d
SPiersiak <50294425+SPiersiak@users.noreply.github.com>
2019-06-18 13:28:17 +0200

    gotowy projekt z testami jednostkowymi

--- Aplikacja/LogTest/MainWindowTest.cs
+++ Aplikacja/LogTest/MainWindowTest.cs
@@ -10,6 +10,22 @@ namespace LogTest
         [TestMethod]
         public void TestMethod1()
         {
+            //arrange
+            string login = "login";
+            string haslo = "haslo";
+            int oczekiwany_wynik = 1;
+            int test;
+            //act
+            if (((login == "") && (haslo == "")) || (login == "") || (haslo == ""))
+            {
+                test = 0;
+            }
+            else
+            {
+                test = 1;
+            }
+            //assert
+            Assert.AreEqual(oczekiwany_wynik, test, 0.001, "Not Correctly");
         }
     }
 }
